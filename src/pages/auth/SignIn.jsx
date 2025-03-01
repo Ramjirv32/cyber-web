29b5bc62030495a8de1b90aeb1d0b366241f9e6f
ramji <ramjib2311@gmail.com>
2025-03-01 11:38:12 +0530

    done

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -31,7 +31,7 @@ export default function Signup() {
         return;
       }
 
-      const response = await fetch('http://localhost:5000/signin', {
+      const response = await fetch('/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -80,7 +80,7 @@ export default function Signup() {
       
       const result = await signInWithPopup(auth, provider);
       
-      const response = await axios.post(`http://localhost:5000/signin`, {
+      const response = await axios.post(`/signin`, {
         email: result.user.email,
         password: result.user.uid
       });
